53150d6d14e8d37c485ac41188f537c549134829
Markus Makela <markus.makela@mariadb.com>
2016-05-20 00:03:19 +0300

    Make mxs722 output more clear
    The test now prints subtest numbers.

--- mxs722.cpp
+++ mxs722.cpp
@@ -23,18 +23,21 @@ int main(int argc, char *argv[])
     /** Get a baseline result with a good configuration */
     int baseline = test->ssh_maxscale(true, "maxscale -c --user=maxscale ");
 
+    printf("Test 1\n");
     /** Create a type error in the configuration and see if it is noticed */
     test->ssh_maxscale(true, "sed -i -e 's/service/ecivres/' /etc/maxscale.cnf");
     test->add_result(baseline == test->ssh_maxscale(true, "maxscale -c --user=maxscale "),
                      "Configuration error should be detected.\n");
     test->ssh_maxscale(true, "sed -i -e 's/ecivres/service/' /etc/maxscale.cnf");
 
+    printf("Test 2\n");
     /** Add unknown characters to configuration */
     test->ssh_maxscale(true, "sed -i -e 's/^/?/' /etc/maxscale.cnf");
     test->add_result(baseline == test->ssh_maxscale(true, "maxscale -c --user=maxscale "),
                      "Configuration error should be detected.\n");
     test->ssh_maxscale(true, "sed -i -e 's/^?//' /etc/maxscale.cnf");
 
+    printf("Test 3\n");
     /** Comment everything out */
     test->ssh_maxscale(true, "sed -i -e 's/#.*//' /etc/maxscale.cnf");
     test->ssh_maxscale(true, "sed -i -e 's/^/#/' /etc/maxscale.cnf");
@@ -42,6 +45,7 @@ int main(int argc, char *argv[])
                      "Configuration error should be detected.\n");
     test->ssh_maxscale(true, "sed -i -e 's/^#//' /etc/maxscale.cnf");
 
+    printf("Test 4\n");
     /** Add an empty section */
     test->ssh_maxscale(true, "sed -i -e 's/\\[.*\\]/[]/' /etc/maxscale.cnf");
     test->ssh_maxscale(true, "sed -i -e 's/^/#/' /etc/maxscale.cnf");
