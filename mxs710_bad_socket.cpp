f79a9ae0a67ff647641f5b074411be265275e958
Timofey Turenko <timofey.turenko@skysql.com>
2016-05-25 16:50:01 +0300

    add mxs-710 test

---
+++ mxs710_bad_socket.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file mxs710_bad_socket.cpp mxs710_bad_socket regression case (Maxscale does not startup properly and crashes after trying to login to database)
+ * - try to start maxscale with "socket=/var/lib/mysqld/mysql.sock" in the listener definition
+ * - do not expect crash
+ */
+
+
+#include <my_config.h>
+#include <iostream>
+#include <unistd.h>
+#include "testconnections.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    TestConnections * Test = new TestConnections(argc, argv);
+    Test->set_timeout(10);
+
+    Test->check_log_err((char *) "Unsupported router option \"slave\"", TRUE);
+
+    Test->check_maxscale_alive();
+    Test->copy_all_logs(); return(Test->global_result);
+}
+
