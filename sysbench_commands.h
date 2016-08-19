23d4e80a9652568d40a1924acc2dda1061621080
Timofey Turenko <timofey.turenko@skysql.com>
2016-08-19 11:09:37 +0300

    add sysbench example

--- sysbench_commands.h
+++ sysbench_commands.h
@@ -53,5 +53,19 @@ const char * sysbench_command =
                 --max-requests=0 --report-interval=5 --max-time=2592000 run";
 
 
+                const char * sysbench_prepare_short =
+                         "%s/sysbench --test=%s/tests/db/oltp.lua \
+                         --oltp-table-size=10000 --mysql-db=test --mysql-user=skysql --mysql-password=skysql \
+                         --mysql-port=4006 --mysql-host=%s --oltp-tables-count=4 prepare";
+
+                const char * sysbench_command_short =
+                        "%s/sysbench --test=%s/tests/db/oltp.lua \
+                        --mysql-host=%s --mysql-port=%d --mysql-user=skysql --mysql-password=skysql \
+                        --mysql-db=test --mysql-table-engine=innodb --mysql-ignore-duplicates=on \
+                        --num-threads=32 --oltp-table-size=10000 --oltp-tables-count=2 --oltp-read-only=%s \
+                        --oltp-dist-type=uniform --oltp-skip-trx=off --init-rng=on --oltp-test-mode=complex \
+                        --max-requests=0 --report-interval=5 --max-time=300 run";
+
+
 
 #endif // SYSBENCH_COMMANDS_H
