0d6b09fe3f934600c328c38a0d5b422c3aff0b5b
Markus Makela <markus.makela@mariadb.com>
2016-10-04 14:37:05 +0300

    Add failover test for mysqlmon
    Test the new failover functionality in the MySQL monitor.

---
+++ failover_mysqlmon.cpp
@@ -0,0 +1,58 @@
+/**
+ * MySQL Monitor Failover Test
+ */
+
+#include <my_config.h>
+#include <iostream>
+#include "testconnections.h"
+
+int main(int argc, char *argv[])
+{
+    TestConnections * test = new TestConnections(argc, argv);
+
+    /** Create the test table and insert some data */
+    test->connect_maxscale();
+    test->try_query("CREATE OR REPLACE TABLE test.t1 (id int)");
+    test->try_query("INSERT INTO test.t1 VALUES (1)");
+    test->close_maxscale_connections();
+
+    /** Block all but one node */
+    test->repl->block_node(0);
+    test->repl->block_node(1);
+    test->repl->block_node(2);
+
+    /** Wait for the monitor to detect it */
+    sleep(15);
+
+    /** Connect and insert should work */
+    test->connect_maxscale();
+    test->try_query("INSERT INTO test.t1 VALUES (1)");
+    test->close_maxscale_connections();
+
+    /** Unblock nodes */
+    test->repl->unblock_node(0);
+    test->repl->unblock_node(1);
+    test->repl->unblock_node(2);
+
+    /** Wait for the monitor to detect it */
+    sleep(15);
+
+    /**
+     * Check that we are still using the last node to which we failed over
+     * to and that the old nodes are in maintenance mode
+     */
+    test->connect_maxscale();
+    test->try_query("INSERT INTO test.t1 VALUES (1)");
+    char maxscale_id[256], real_id[256];
+    find_field(test->conn_rwsplit, "SELECT @@server_id", "@@server_id", maxscale_id);
+    find_field(test->repl->nodes[3], "SELECT @@server_id", "@@server_id", real_id);
+    test->add_result(strcmp(maxscale_id, real_id) != 0,
+                     "@@server_id is different: %s != %s", maxscale_id, real_id);
+    test->close_maxscale_connections();
+
+    /** Check that MaxScale is running */
+    test->add_result(test->check_maxscale_alive(), "MaxScale is not running");
+
+    test->copy_all_logs();
+    return test->global_result;
+}
