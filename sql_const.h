317efd1e002798103f5140a51132ad2d9a2e20d1
Markus Makela <markus.makela@mariadb.com>
2016-11-01 14:31:46 +0200

    Clean up SQL statements
    The SQL statements are now a bit cleaner.

--- sql_const.h
+++ sql_const.h
@@ -2,24 +2,24 @@
 #define SQL_CONST_H
 
 const char * create_repl_user =
-        "grant replication slave on *.* to repl@'%%' identified by 'repl';\
-        FLUSH PRIVILEGES";
+        "grant replication slave on *.* to repl@'%%' identified by 'repl'; "
+        "FLUSH PRIVILEGES";
 const char * setup_slave =
-        "change master to MASTER_HOST='%s',\
-         MASTER_USER='repl',\
-         MASTER_PASSWORD='repl',\
-         MASTER_LOG_FILE='%s',\
-         MASTER_LOG_POS=%s,\
-         MASTER_PORT=%d, MASTER_CONNECT_RETRY=1;\
-        start slave;";
+        "change master to MASTER_HOST='%s', "
+        "MASTER_USER='repl', "
+        "MASTER_PASSWORD='repl', "
+        "MASTER_LOG_FILE='%s', "
+        "MASTER_LOG_POS=%s, "
+        "MASTER_PORT=%d, MASTER_CONNECT_RETRY=1; "
+        "start slave;";
 
 const char * setup_slave_no_pos =
-        "change master to MASTER_HOST='%s',\
-         MASTER_USER='repl',\
-         MASTER_PASSWORD='repl',\
-         MASTER_LOG_FILE='mar-bin.000001',\
-         MASTER_LOG_POS=4,\
-         MASTER_PORT=%d, MASTER_CONNECT_RETRY=1";
+         "change master to MASTER_HOST='%s', "
+         "MASTER_USER='repl', "
+         "MASTER_PASSWORD='repl', "
+         "MASTER_LOG_FILE='mar-bin.000001', "
+         "MASTER_LOG_POS=4, "
+         "MASTER_PORT=%d, MASTER_CONNECT_RETRY=1";
 
 
 #endif // SQL_CONST_H
