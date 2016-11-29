c09ca73ed09880557e579f3428b8074d597abac5
Timofey Turenko <timofey.turenko@skysql.com>
2016-11-29 19:14:29 +0200

    add check for maxskysql user

--- mariadb_nodes.cpp
+++ mariadb_nodes.cpp
@@ -437,6 +437,13 @@ int Mariadb_nodes::check_replication(int master)
     get_versions();
 
     for (int i = 0; i < N; i++) {
+        conn = open_conn(port[i], IP[i], "maxskysql", "skysql", ssl);
+        if (mysql_errno(conn) != 0) {
+            printf("Error connectiong node %d with maxskysql user\n", i); fflush(stdout);
+            res1 = 1;
+        }
+        if (conn != NULL ) {mysql_close(conn);}
+
         conn = open_conn(port[i], IP[i], user_name, password, ssl);
         if (mysql_errno(conn) != 0) {
             printf("Error connectiong node %d\n", i); fflush(stdout);
@@ -504,6 +511,12 @@ int Mariadb_nodes::check_galera()
     printf("Checking Galera\n"); fflush(stdout);
     get_versions();
     for (int i = 0; i < N; i++) {
+        conn = open_conn(port[i], IP[i], "maxskysql", "skysql", ssl);
+        if (mysql_errno(conn) != 0) {
+            printf("Error connectiong node %d with maxskysql user\n", i); fflush(stdout);
+            res1 = 1;
+        }
+        if (conn != NULL ) {mysql_close(conn);}
         conn = open_conn(port[i], IP[i], user_name, password, ssl);
         if (mysql_errno(conn) != 0) {
             printf("Error connectiong node %d\n", i);
