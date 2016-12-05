dcc7c2af984b2fbef97d9595c749cd0eb99990bc
Markus Makela <markus.makela@mariadb.com>
2016-12-05 14:24:27 +0200

    Add test for MXS-1045
    The test checks for zombie processes.

---
+++ mxs1045.cpp
@@ -0,0 +1,27 @@
+/**
+* MXS-1045: Defunct processes after maxscale have executed script during failover
+*
+* Check that script execution doesn't leave zombie processes
+*/
+#include "testconnections.h"
+
+int main(int argc, char *argv[])
+{
+    TestConnections * test = new TestConnections(argc, argv);
+
+    test->tprintf("Block master");
+    test->repl->block_node(0);
+
+    test->tprintf("Wait for monitor to see it");
+    sleep(10);
+
+    test->tprintf("Check that there are no zombies");
+
+    int res = test->ssh_maxscale_sh(false,
+                                    "if [ \"`ps -ef|grep defunct|grep -v grep`\" != \"\" ]; then exit 1; fi");
+    test->add_result(res, "Zombie processes were found");
+
+    test->repl->unblock_node(0);
+
+    return test->global_result;
+}
