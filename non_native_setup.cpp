0813412117cc999a1808ede4db208aff2ace5aaa
Markus Makela <markus.makela@mariadb.com>
2016-12-06 07:58:37 +0200

    Added small sleep to non_native_setup
    Adding a small sleep seems to better catch debug assertions.

--- non_native_setup.cpp
+++ non_native_setup.cpp
@@ -11,15 +11,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2)
-  {
-    return 1;
-  }
+    if (argc < 2)
+    {
+        return 1;
+    }
 
-  int local_argc = argc - 1;
-  char** local_argv = &argv[1];
+    int local_argc = argc - 1;
+    char** local_argv = &argv[1];
 
-  TestConnections * Test = new TestConnections(local_argc, local_argv);
+    TestConnections * Test = new TestConnections(local_argc, local_argv);
 
-  return 0;
+    sleep(3);
+
+    return 0;
 }
