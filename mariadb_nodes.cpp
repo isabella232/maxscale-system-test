0d3e60d00f3248b44e320e00f66d84eac8d8fc1a
Markus Makela <markus.makela@mariadb.com>
2017-01-19 09:50:57 +0200

    Remove grastate.dat when stopping Galera nodes
    This should prevent Galera nodes from being corrupted after a restart.

--- mariadb_nodes.cpp
+++ mariadb_nodes.cpp
@@ -305,6 +305,9 @@ int Galera_nodes::start_galera()
     int local_result = 0;
     local_result += stop_nodes();
 
+    // Remove the grastate.dat file
+    ssh_node(0, "rm -f /var/lib/mysql/grastate.dat", true);
+
     printf("Starting new Galera cluster\n");  fflush(stdout);
     local_result += start_node(0, (char *) " --wsrep-cluster-address=gcomm://");
 
