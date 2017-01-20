def6588813e7fb0a7eacbea8641e494531363ad3
Markus Makela <markus.makela@mariadb.com>
2017-01-20 10:33:48 +0200

    Refactor mysqltest based tests
    The tests now use a generic test driver.
    
    Fixed galera test not actually using galera template. The result files
    also relied on outdated information about the test environment.

--- mariadb_tests_hartmut.sh
+++ mariadb_tests_hartmut.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-#set -x
+# This is required by one of the tests
+#
+# TODO: Don't test correctness of routing with mysqltest
+#
+./non_native_setup $1
+master_id=`echo "SELECT @@server_id" | mysql -u$node_user -p$node_password -h $node_000_network $ssl_options| tail -n1`
+echo "--disable_query_log" > Hartmut_tests/maxscale-mysqltest/testconf.inc
+echo "SET @TMASTER_ID=$master_id;" >> Hartmut_tests/maxscale-mysqltest/testconf.inc
+echo "--enable_query_log" >> Hartmut_tests/maxscale-mysqltest/testconf.inc
 
-rp=`realpath $0`
-export test_dir=`dirname $rp`
-export test_name=`basename $rp`
-$test_dir/non_native_setup $test_name
-export ssl_options="--ssl-cert=$test_dir/ssl-cert/client-cert.pem --ssl-key=$test_dir/ssl-cert/client-key.pem"
-#$test_dir/configure_maxscale.sh 
-if [ $? -ne 0 ] ; then 
-	echo "configure_maxscale.sh failed"
-	exit 1
-fi
-#sleep 15
-
-export Master_id=`echo "SELECT (@@server_id)" | mysql -u$node_user -p$node_password -h $node_000_network $ssl_options| tail -n1`
-cat ./maxscale-mysqltest/fail.txt | grep "FAILED"
-
-echo "Maister_id $Master_id"
-$test_dir/Hartmut_tests/mariadb_tests_hartmut_imp 4006
-
-cat $test_dir/Hartmut_tests/maxscale-mysqltest/fail.txt | grep "FAILED"
-
-if [ $? -ne 0 ]; then
-	cat $test_dir/Hartmut_tests/maxscale-mysqltest/fail.txt | grep "PASSED"
-	if [ $? -ne 0 ]; then
-		res=1
-	else 
-		res=0
-	fi
-else
-	res=1
-fi
-
-$test_dir/copy_logs.sh mariadb_tests_hartmut
-exit $res
+./mysqltest_driver.sh $1 Hartmut_tests/maxscale-mysqltest 4006
