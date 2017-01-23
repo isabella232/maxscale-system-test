467af8121779a6f29ebc26da32ed1ea5891a6cf2
Markus Makela <markus.makela@mariadb.com>
2017-01-23 14:02:00 +0200

    Use `galera` template for bug676
    Made the test a bit simpler. It also uses a common Galera template as it
    doesn't require anything special.
    
    Added `monitor_interval` to the generic Galera template to make tests a
    bit faster.

--- bug676.cpp
+++ bug676.cpp
@@ -2,9 +2,9 @@
  * @file bug676.cpp  reproducing attempt for bug676
  * - connect to RWSplit
  * - stop node0
- * - sleep 30 seconds
+ * - sleep 20 seconds
  * - reconnect
- * - check if 'USE test ' is ok
+ * - check if 'USE test' is ok
  * - check MaxScale is alive
  */
 
@@ -15,35 +15,28 @@
 int main(int argc, char *argv[])
 {
     TestConnections test(argc, argv);
-    test.set_timeout(20);
 
-    MYSQL * conn = open_conn_no_db(test.rwsplit_port, test.maxscale_IP, test.maxscale_user,
-                                   test.maxscale_password, test.ssl);
+    test.set_timeout(30);
 
-    test.tprintf("Stopping %d\n", 0);
+    test.connect_maxscale();
+    test.tprintf("Stopping node 0");
     test.galera->block_node(0);
+    test.close_maxscale_connections();
 
     test.stop_timeout();
-    sleep(30);
-    test.set_timeout(20);
-    mysql_close(conn);
 
-    conn = open_conn_no_db(test.rwsplit_port, test.maxscale_IP, test.maxscale_user, test.maxscale_password,
-                           test.ssl);
+    test.tprintf("Waiting until the monitor picks a new master");
+    sleep(20);
 
-    test.add_result(conn == 0, "Error connection to RW Split\n");
+    test.set_timeout(30);
 
-    test.tprintf("selecting DB 'test' for rwsplit\n");
-    test.try_query(conn, "USE test");
-
-    test.tprintf("Closing connection\n");
-    mysql_close(conn);
-
-    test.connect_rwsplit();
+    test.connect_maxscale();
+    test.try_query(test.conn_rwsplit, "USE test");
     test.try_query(test.conn_rwsplit, "show processlist;");
     test.close_maxscale_connections();
 
     test.stop_timeout();
+
     test.galera->unblock_node(0);
 
     return test.global_result;
