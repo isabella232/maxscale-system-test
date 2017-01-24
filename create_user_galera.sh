f65cdd86bf9abdb9c9eb4286d8e025b468fc5e17
Markus Makela <markus.makela@mariadb.com>
2017-01-24 23:34:28 +0200

    Refactor Galera user creation
    The users are now created each time the cluster is restarted. This removes
    the need to do the user creation when the testing environment is being
    initialized.

--- create_user_galera.sh
+++ create_user_galera.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-echo "DROP USER '$galera_user'@'%'" | sudo mysql
-echo "grant all privileges on *.*  to '$galera_user'@'%' identified by '$galera_password' WITH GRANT OPTION" 
-echo "grant all privileges on *.*  to '$galera_user'@'%' identified by '$galera_password' WITH GRANT OPTION" | sudo mysql
+# Wait until the node is ready
 
-echo "grant all privileges on *.*  to 'maxskysql'@'%' identified by 'skysql' WITH GRANT OPTION" | sudo mysql
+for ((i=0;i<100;i++))
+do
+    mysql -ss -u root -e 'show status like "wsrep_ready"' | grep 'ON' && break || sleep 1
+done
+
+# The removal of the anonymous ''@'localhost' user is done in a somewhat crude
+# way. The proper way would be to do a secure installation or drop the users
+# with DROP USER statements.
+
+mysql -u root --force <<EOF
+
+DELETE FROM mysql.user WHERE user = '';
+
+DROP USER '$galera_user'@'%';
+CREATE USER '$galera_user'@'%' IDENTIFIED BY '$galera_password';
+GRANT ALL PRIVILEGES ON *.* TO '$galera_user'@'%' WITH GRANT OPTION;
+
+DROP USER 'maxskysql'@'%';
+CREATE USER 'maxskysql'@'%' IDENTIFIED BY 'skysql';
+GRANT ALL PRIVILEGES ON *.* TO 'maxskysql'@'%' WITH GRANT OPTION;
+
+DROP USER 'repl'@'%';
+CREATE USER 'repl'@'%' IDENTIFIED BY 'repl';
+GRANT ALL PRIVILEGES ON *.* TO 'repl'@'%' WITH GRANT OPTION;
+
+DROP USER 'skysql'@'%';
+CREATE USER 'skysql'@'%' IDENTIFIED BY 'skysql';
+GRANT ALL PRIVILEGES ON *.* TO 'skysql'@'%' WITH GRANT OPTION;
+
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+EOF
