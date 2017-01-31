752158e97c63b71a93667544681ab47adc08e71d
Markus Makela <markus.makela@mariadb.com>
2017-01-31 15:24:01 +0200

    Clean up short_sessions
    Cleaned up the test and updated comments.

--- short_sessions.cpp
+++ short_sessions.cpp
@@ -3,9 +3,8 @@
  *
  * - using RSplit create table
  * - close connection
- * - do 10000 times: open connections to RWSplit, execute short INSERT, close connection
- * - do 10000 times: open connection to RWSplit, execute short SELECT, close connection
- * - repeat last previous step also to ReadConn master and ReadConn slave
+ * - do 100 times: open connections to RWSplit, execute short INSERT, close connection
+ * - Select inserted rows through all services
  * - check if Maxscale alive
  */
 
@@ -18,60 +17,53 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int iterations = 10000;
+    int iterations = 100;
 
-    TestConnections * Test = new TestConnections(argc, argv);
-    if (Test->smoke)
-    {
-        iterations = 100;
-    }
-    Test->set_timeout(20);
+    TestConnections test(argc, argv);
+    test.set_timeout(20);
+    test.repl->connect();
 
-    Test->repl->connect();
-
-    MYSQL * conn;
-    char sql[100];
-
-    conn = Test->open_rwsplit_connection();
-    execute_query(conn, (char *) "DROP DATABASE IF EXISTS test;");
-    execute_query(conn, (char *) "CREATE DATABASE test; USE test;");
-    execute_query(conn, (char *) "USE test_non_existing_DB; USE test;");
+    MYSQL *conn = test.open_rwsplit_connection();
+    execute_query(conn, "DROP DATABASE IF EXISTS test;");
+    execute_query(conn, "CREATE DATABASE test;");
+    execute_query(conn, "USE test;");
     create_t1(conn);
     mysql_close(conn);
-    Test->tprintf("Table t1 is created\n");
+
+    test.tprintf("Executing %d inserts", iterations);
 
     for (int i = 0; i < iterations; i++)
     {
-        Test->set_timeout(15);
-        conn = Test->open_rwsplit_connection();
+        char sql[100];
         sprintf(sql, "INSERT INTO t1 (x1, fl) VALUES(%d, 1);", i);
-        Test->tprintf("%s\n", sql);
+
+        test.set_timeout(15);
+        conn = test.open_rwsplit_connection();
         execute_query(conn, sql);
         mysql_close(conn);
     }
-    Test->set_timeout(20);
-    fflush(stdout);
 
-    Test->set_timeout(20);
-    Test->tprintf("Connecting to MaxScale\n");
-    Test->add_result(Test->connect_maxscale(), "Error connecting to Maxscale\n");
-    Test->tprintf("Checking t1 table using RWSplit router\n");
-    Test->set_timeout(240);
-    Test->add_result( execute_select_query_and_check(Test->conn_rwsplit, (char *) "SELECT * FROM t1;",
-                                                     iterations), "t1 is wrong\n");
-    Test->tprintf("Checking t1 table using ReadConn router in master mode\n");
-    Test->set_timeout(240);
-    Test->add_result(  execute_select_query_and_check(Test->conn_master, (char *) "SELECT * FROM t1;",
-                                                      iterations), "t1 is wrong\n");
-    Test->tprintf("Checking t1 table using ReadConn router in slave mode\n");
-    Test->set_timeout(240);
-    Test->add_result(  execute_select_query_and_check(Test->conn_slave, (char *) "SELECT * FROM t1;", iterations),
-                       "t1 is wrong\n");
-    Test->set_timeout(20);
-    Test->close_maxscale_connections();
+    test.set_timeout(20);
+    test.add_result(test.connect_maxscale(), "Failed to connect to MaxScale");
 
-    Test->check_maxscale_alive();
+    test.tprintf("Checking t1 table using RWSplit router");
+    test.set_timeout(240);
+    test.add_result(execute_select_query_and_check(test.conn_rwsplit, (char *) "SELECT * FROM t1;",
+                                                   iterations), "t1 is wrong");
 
-    Test->copy_all_logs();
-    return Test->global_result;
+    test.tprintf("Checking t1 table using ReadConn router in master mode");
+    test.set_timeout(240);
+    test.add_result(execute_select_query_and_check(test.conn_master, (char *) "SELECT * FROM t1;",
+                                                   iterations), "t1 is wrong");
+
+    test.tprintf("Checking t1 table using ReadConn router in slave mode");
+    test.set_timeout(240);
+    test.add_result(execute_select_query_and_check(test.conn_slave, (char *) "SELECT * FROM t1;", iterations),
+                    "t1 is wrong");
+
+    test.set_timeout(20);
+    test.close_maxscale_connections();
+    test.check_maxscale_alive();
+
+    return test.global_result;
 }
