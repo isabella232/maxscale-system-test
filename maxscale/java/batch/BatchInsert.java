499f2d7672b574dc2b06606f5ea65ba157f83bea
Markus Makela <markus.makela@mariadb.com>
2017-04-03 23:07:17 +0300

    Revert "Extend batch insert test for MXS-1203"
    This reverts commit 7c0d4477ce54a1df7d8fa8082fee6950bd9cadd8. SELECT
    statements aren't supported by all JDBC implementations and this includes
    the one we use for testing.

--- maxscale/java/batch/BatchInsert.java
+++ maxscale/java/batch/BatchInsert.java
@@ -3,54 +3,45 @@ package maxscale.java.batch;
 import maxscale.java.MaxScaleConfiguration;
 import maxscale.java.MaxScaleConnection;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class BatchInsert {
 
     public static void main(String[] args) {
+        boolean error = false;
         try {
-
             MaxScaleConfiguration config = new MaxScaleConfiguration("batchinsert");
             MaxScaleConnection maxscale = new MaxScaleConnection("useBatchMultiSendNumber=500");
 
-            Connection connection = maxscale.getConnRw();
-            Statement stmt = connection.createStatement();
-            stmt.execute("CREATE OR REPLACE TABLE tt (d int)");
+            try {
+                Connection connection = maxscale.getConnRw();
+                Statement stmt = connection.createStatement();
+            
+                stmt.execute("DROP TABLE IF EXISTS tt");
+                stmt.execute("CREATE TABLE tt (d int)");
 
-            for (int i = 0; i < 300; i++) {
-                switch (i % 4) {
-                case 0:
-                    stmt.addBatch("SET @test2='aaa'");
-                    break;
-
-                case 1:
+                for (int i = 0; i < 150; i++) {
                     stmt.addBatch("INSERT INTO tt(d) VALUES (1)");
-                    break;
 
-                case 2:
-                    stmt.addBatch("SELECT * FROM tt");
-                    break;
-
-                case 3:
-                    stmt.addBatch("SET @test2=(SELECT SLEEP 0.1)");
-                    break;
+                    if (i % 3 == 0) {
+                        stmt.addBatch("SET @test2='aaa'");
+                    }
                 }
+
+                stmt.executeBatch();
+                System.out.println("finished");
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                error = true;
             }
-
-            stmt.executeBatch();
-
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM tt WHERE d <> ?");
-
-            for (int i = 0; i < 300; i++) {
-                ps.setInt(1, i);
-                ps.addBatch();
-            }
-
-            ps.executeBatch();
+            config.close();
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+        }
+
+        if (error) {
             System.exit(1);
         }
     }
